de8f526124e4febb8b7c2edbc086653956a2cfe2
Suresh Ramamurthy <suresh@aporeto.com>
2016-09-24 17:20:21 -0700

    Fixed: Added models for clair webhook support

--- python/gaia/__init__.py
+++ python/gaia/__init__.py
@@ -2,11 +2,12 @@
 
 
 
-__all__ = ['APIAuthorizationPolicy', 'Authenticator', 'Certificate', 'ComputedDependencyMapView', 'ComputedPolicy', 'DependencyMap', 'DependencyMapSubview', 'DependencyMapView', 'ExternalService', 'FileAccessPolicy', 'FilePath', 'FlowStatistic', 'HealthReport', 'Image', 'Layer', 'MapEdge', 'MapNode', 'MyNamespace', 'Namespace', 'NamespaceMappingPolicy', 'NetworkAccessPolicy', 'Policy', 'PolicyRule', 'ProcessingUnit', 'RenderedPolicy', 'Root', 'Server', 'SystemCall', 'Tag', 'User', 'Vulnerability', 'routes']
+__all__ = ['APIAuthorizationPolicy', 'Authenticator', 'Certificate', 'Clairnotification', 'ComputedDependencyMapView', 'ComputedPolicy', 'DependencyMap', 'DependencyMapSubview', 'DependencyMapView', 'ExternalService', 'FileAccessPolicy', 'FilePath', 'FlowStatistic', 'HealthReport', 'Image', 'Layer', 'MapEdge', 'MapNode', 'MyNamespace', 'Namespace', 'NamespaceMappingPolicy', 'NetworkAccessPolicy', 'Notification', 'Policy', 'PolicyRule', 'ProcessingUnit', 'RenderedPolicy', 'Root', 'Server', 'Service', 'SystemCall', 'Tag', 'User', 'Vulnerability', 'VulnerabilityWithlayers', 'routes']
 
 from .apiauthorizationpolicy import APIAuthorizationPolicy
 from .authenticator import Authenticator
 from .certificate import Certificate
+from .clairnotification import Clairnotification
 from .computeddependencymapview import ComputedDependencyMapView
 from .computedpolicy import ComputedPolicy
 from .dependencymap import DependencyMap
@@ -25,14 +26,17 @@ from .mynamespace import MyNamespace
 from .namespace import Namespace
 from .namespacemappingpolicy import NamespaceMappingPolicy
 from .networkaccesspolicy import NetworkAccessPolicy
+from .notification import Notification
 from .policy import Policy
 from .policyrule import PolicyRule
 from .processingunit import ProcessingUnit
 from .renderedpolicy import RenderedPolicy
 from .root import Root
 from .server import Server
+from .service import Service
 from .systemcall import SystemCall
 from .tag import Tag
 from .user import User
 from .vulnerability import Vulnerability
+from .vulnerabilitywithlayers import VulnerabilityWithlayers
 from .routes import routes
