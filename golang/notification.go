67c475c2ee20498fb3400eab8aee64504c701e14
Suresh Ramamurthy <suresh@aporeto.com>
2016-09-25 17:18:19 -0700

    Fixed: updated clair vulneralibility spec

--- golang/notification.go
+++ golang/notification.go
@@ -3,6 +3,9 @@ package gaia
 import "fmt"
 import "github.com/aporeto-inc/elemental"
 
+import "time"
+import "github.com/aporeto-inc/gaia/golang/constants"
+
 // NotificationIdentity represents the Identity of the object
 var NotificationIdentity = elemental.Identity{
 	Name:     "notification",
@@ -14,20 +17,32 @@ type NotificationsList []*Notification
 
 // Notification represents the model of a notification
 type Notification struct {
-	// Created is the time when the notification was created
-	Created string `json:"Created" cql:"created,omitempty"`
-
 	// ID is the identifier of the object.
 	ID string `json:"ID" cql:"id,primarykey,omitempty"`
 
+	// Annotation stores additional information about an entity
+	Annotation map[string]string `json:"annotation" cql:"annotation,omitempty"`
+
+	// AssociatedTags are the list of tags attached to an entity
+	AssociatedTags []string `json:"associatedTags" cql:"associatedtags,omitempty"`
+
+	// CreatedAt is the time at which an entity was created
+	CreatedAt time.Time `json:"createdAt" cql:"createdat,omitempty"`
+
+	// Deleted marks if the entity has been deleted.
+	Deleted bool `json:"-" cql:"deleted,omitempty"`
+
 	// Deleted is the time when the notification was deleted
-	Deleted string `json:"deleted" cql:"deleted,omitempty"`
+	DeletedAt time.Time `json:"deletedAt" cql:"deletedat,omitempty"`
 
 	// Limits the amount of results in the "LayersIntroducingVulnerability" property on New and Old vulnerabilities
 	Limit string `json:"limit" cql:"limit,omitempty"`
 
 	// Name is the name of the notification
 	Name string `json:"name" cql:"name,omitempty"`
+
+	// Namespace tag attached to an entity
+	Namespace string `json:"namespace" cql:"namespace,primarykey,omitempty"`
 
 	// New is the new layers that introduced vulnerability
 	New *VulnerabilityWithLayers `json:"new" cql:"new,omitempty"`
@@ -43,12 +58,26 @@ type Notification struct {
 
 	// Page is the page number
 	Page string `json:"page" cql:"page,omitempty"`
+
+	// ParentID is the ID of the parent, if any,
+	ParentID string `json:"parentID" cql:"parentid,omitempty"`
+
+	// ParentType is the type of the parent, if any. It will be set to the parent's Identity.Name.
+	ParentType string `json:"parentType" cql:"parenttype,omitempty"`
+
+	// Status of an entity
+	Status constants.EntityStatus `json:"status" cql:"status,omitempty"`
+
+	// UpdatedAt is the time at which an entity was updated.
+	UpdatedAt time.Time `json:"updatedAt" cql:"updatedat,omitempty"`
 }
 
 // NewNotification returns a new *Notification
 func NewNotification() *Notification {
 
-	return &Notification{}
+	return &Notification{
+		Status: constants.Active,
+	}
 }
 
 // Identity returns the Identity of the object.
@@ -74,6 +103,76 @@ func (o *Notification) SetIdentifier(ID string) {
 	o.ID = ID
 }
 
+// GetAssociatedTags returns the associatedTags of the receiver
+func (o *Notification) GetAssociatedTags() []string {
+	return o.AssociatedTags
+}
+
+// SetAssociatedTags set the given associatedTags of the receiver
+func (o *Notification) SetAssociatedTags(associatedTags []string) {
+	o.AssociatedTags = associatedTags
+}
+
+// SetCreatedAt set the given createdAt of the receiver
+func (o *Notification) SetCreatedAt(createdAt time.Time) {
+	o.CreatedAt = createdAt
+}
+
+// GetDeleted returns the deleted of the receiver
+func (o *Notification) GetDeleted() bool {
+	return o.Deleted
+}
+
+// SetDeleted set the given deleted of the receiver
+func (o *Notification) SetDeleted(deleted bool) {
+	o.Deleted = deleted
+}
+
+// GetNamespace returns the namespace of the receiver
+func (o *Notification) GetNamespace() string {
+	return o.Namespace
+}
+
+// SetNamespace set the given namespace of the receiver
+func (o *Notification) SetNamespace(namespace string) {
+	o.Namespace = namespace
+}
+
+// GetParentID returns the parentID of the receiver
+func (o *Notification) GetParentID() string {
+	return o.ParentID
+}
+
+// SetParentID set the given parentID of the receiver
+func (o *Notification) SetParentID(parentID string) {
+	o.ParentID = parentID
+}
+
+// GetParentType returns the parentType of the receiver
+func (o *Notification) GetParentType() string {
+	return o.ParentType
+}
+
+// SetParentType set the given parentType of the receiver
+func (o *Notification) SetParentType(parentType string) {
+	o.ParentType = parentType
+}
+
+// GetStatus returns the status of the receiver
+func (o *Notification) GetStatus() constants.EntityStatus {
+	return o.Status
+}
+
+// SetStatus set the given status of the receiver
+func (o *Notification) SetStatus(status constants.EntityStatus) {
+	o.Status = status
+}
+
+// SetUpdatedAt set the given updatedAt of the receiver
+func (o *Notification) SetUpdatedAt(updatedAt time.Time) {
+	o.UpdatedAt = updatedAt
+}
+
 // Validate valides the current information stored into the structure.
 func (o *Notification) Validate() elemental.Errors {
 
@@ -94,16 +193,6 @@ func (o Notification) SpecificationForAttribute(name string) elemental.Attribute
 
 // NotificationAttributesMap represents the map of attribute for Notification.
 var NotificationAttributesMap = map[string]elemental.AttributeSpecification{
-	"Created": elemental.AttributeSpecification{
-		AllowedChoices: []string{},
-		Exposed:        true,
-		Filterable:     true,
-		Format:         "free",
-		Name:           "Created",
-		Orderable:      true,
-		Stored:         true,
-		Type:           "string",
-	},
 	"ID": elemental.AttributeSpecification{
 		AllowedChoices: []string{},
 		Autogenerated:  true,
@@ -119,15 +208,55 @@ var NotificationAttributesMap = map[string]elemental.AttributeSpecification{
 		Type:           "string",
 		Unique:         true,
 	},
+	"Annotation": elemental.AttributeSpecification{
+		AllowedChoices: []string{},
+		Exposed:        true,
+		Name:           "annotation",
+		Stored:         true,
+		SubType:        "annotation",
+		Type:           "external",
+	},
+	"AssociatedTags": elemental.AttributeSpecification{
+		AllowedChoices: []string{},
+		Exposed:        true,
+		Getter:         true,
+		Name:           "associatedTags",
+		Setter:         true,
+		Stored:         true,
+		SubType:        "tags_list",
+		Type:           "external",
+	},
+	"CreatedAt": elemental.AttributeSpecification{
+		AllowedChoices: []string{},
+		Autogenerated:  true,
+		Exposed:        true,
+		Filterable:     true,
+		Name:           "createdAt",
+		Orderable:      true,
+		ReadOnly:       true,
+		Setter:         true,
+		Stored:         true,
+		Type:           "time",
+	},
 	"Deleted": elemental.AttributeSpecification{
+		AllowedChoices: []string{},
+		Autogenerated:  true,
+		Filterable:     true,
+		Getter:         true,
+		Name:           "deleted",
+		Orderable:      true,
+		Setter:         true,
+		Stored:         true,
+		Type:           "boolean",
+	},
+	"DeletedAt": elemental.AttributeSpecification{
 		AllowedChoices: []string{},
 		Exposed:        true,
 		Filterable:     true,
-		Format:         "free",
-		Name:           "deleted",
+		Name:           "deletedAt",
 		Orderable:      true,
 		Stored:         true,
-		Type:           "string",
+		Type:           "time",
 	},
 	"Limit": elemental.AttributeSpecification{
 		AllowedChoices: []string{},
@@ -148,6 +277,23 @@ var NotificationAttributesMap = map[string]elemental.AttributeSpecification{
 		Orderable:      true,
 		Stored:         true,
 		Type:           "string",
+	},
+	"Namespace": elemental.AttributeSpecification{
+		AllowedChoices: []string{},
+		Autogenerated:  true,
+		CreationOnly:   true,
+		Exposed:        true,
+		Filterable:     true,
+		Format:         "free",
+		Getter:         true,
+		Name:           "namespace",
+		Orderable:      true,
+		PrimaryKey:     true,
+		ReadOnly:       true,
+		Setter:         true,
+		Stored:         true,
+		Type:           "string",
+		Unique:         true,
 	},
 	"New": elemental.AttributeSpecification{
 		AllowedChoices: []string{},
@@ -198,5 +344,59 @@ var NotificationAttributesMap = map[string]elemental.AttributeSpecification{
 		Orderable:      true,
 		Stored:         true,
 		Type:           "string",
+	},
+	"ParentID": elemental.AttributeSpecification{
+		AllowedChoices: []string{},
+		Autogenerated:  true,
+		Exposed:        true,
+		Filterable:     true,
+		ForeignKey:     true,
+		Format:         "free",
+		Getter:         true,
+		Name:           "parentID",
+		Orderable:      true,
+		ReadOnly:       true,
+		Setter:         true,
+		Stored:         true,
+		Type:           "string",
+	},
+	"ParentType": elemental.AttributeSpecification{
+		AllowedChoices: []string{},
+		Autogenerated:  true,
+		Exposed:        true,
+		Filterable:     true,
+		Format:         "free",
+		Getter:         true,
+		Name:           "parentType",
+		Orderable:      true,
+		ReadOnly:       true,
+		Setter:         true,
+		Stored:         true,
+		Type:           "string",
+	},
+	"Status": elemental.AttributeSpecification{
+		AllowedChoices: []string{},
+		Autogenerated:  true,
+		Exposed:        true,
+		Filterable:     true,
+		Getter:         true,
+		Name:           "status",
+		Orderable:      true,
+		Setter:         true,
+		Stored:         true,
+		SubType:        "status_enum",
+		Type:           "external",
+	},
+	"UpdatedAt": elemental.AttributeSpecification{
+		AllowedChoices: []string{},
+		Autogenerated:  true,
+		Exposed:        true,
+		Filterable:     true,
+		Name:           "updatedAt",
+		Orderable:      true,
+		ReadOnly:       true,
+		Setter:         true,
+		Stored:         true,
+		Type:           "time",
 	},
 }
--- python/gaia/notification.py
+++ python/gaia/notification.py
@@ -28,27 +28,43 @@ class Notification(RESTObject):
 
         # Read/Write Attributes
         
-        self._created = None
         self._id = None
+        self._annotation = None
+        self._associatedtags = None
+        self._createdat = None
         self._deleted = None
+        self._deletedat = None
         self._limit = None
         self._name = None
+        self._namespace = None
         self._new = None
         self._nextpage = None
         self._notified = None
         self._old = None
         self._page = None
+        self._parentid = None
+        self._parenttype = None
+        self._status = None
+        self._updatedat = None
         
-        self.expose_attribute(local_name="Created", remote_name="Created")
         self.expose_attribute(local_name="ID", remote_name="ID")
+        self.expose_attribute(local_name="annotation", remote_name="annotation")
+        self.expose_attribute(local_name="associatedTags", remote_name="associatedTags")
+        self.expose_attribute(local_name="createdAt", remote_name="createdAt")
         self.expose_attribute(local_name="deleted", remote_name="deleted")
+        self.expose_attribute(local_name="deletedAt", remote_name="deletedAt")
         self.expose_attribute(local_name="limit", remote_name="limit")
         self.expose_attribute(local_name="name", remote_name="name")
+        self.expose_attribute(local_name="namespace", remote_name="namespace")
         self.expose_attribute(local_name="new", remote_name="new")
         self.expose_attribute(local_name="nextPage", remote_name="nextPage")
         self.expose_attribute(local_name="notified", remote_name="notified")
         self.expose_attribute(local_name="old", remote_name="old")
         self.expose_attribute(local_name="page", remote_name="page")
+        self.expose_attribute(local_name="parentID", remote_name="parentID")
+        self.expose_attribute(local_name="parentType", remote_name="parentType")
+        self.expose_attribute(local_name="status", remote_name="status")
+        self.expose_attribute(local_name="updatedAt", remote_name="updatedAt")
 
         self._compute_args(**kwargs)
 
@@ -72,28 +88,6 @@ class Notification(RESTObject):
 
     # Properties
     @property
-    def Created(self):
-        """ Get Created value.
-
-          Notes:
-              Created is the time when the notification was created
-
-              
-        """
-        return self._created
-
-    @Created.setter
-    def Created(self, value):
-        """ Set Created value.
-
-          Notes:
-              Created is the time when the notification was created
-
-              
-        """
-        self._created = value
-    
-    @property
     def ID(self):
         """ Get ID value.
 
@@ -116,11 +110,77 @@ class Notification(RESTObject):
         self._id = value
     
     @property
+    def annotation(self):
+        """ Get annotation value.
+
+          Notes:
+              Annotation stores additional information about an entity
+
+              
+        """
+        return self._annotation
+
+    @annotation.setter
+    def annotation(self, value):
+        """ Set annotation value.
+
+          Notes:
+              Annotation stores additional information about an entity
+
+              
+        """
+        self._annotation = value
+    
+    @property
+    def associatedTags(self):
+        """ Get associatedTags value.
+
+          Notes:
+              AssociatedTags are the list of tags attached to an entity
+
+              
+        """
+        return self._associatedtags
+
+    @associatedTags.setter
+    def associatedTags(self, value):
+        """ Set associatedTags value.
+
+          Notes:
+              AssociatedTags are the list of tags attached to an entity
+
+              
+        """
+        self._associatedtags = value
+    
+    @property
+    def createdAt(self):
+        """ Get createdAt value.
+
+          Notes:
+              CreatedAt is the time at which an entity was created
+
+              
+        """
+        return self._createdat
+
+    @createdAt.setter
+    def createdAt(self, value):
+        """ Set createdAt value.
+
+          Notes:
+              CreatedAt is the time at which an entity was created
+
+              
+        """
+        self._createdat = value
+    
+    @property
     def deleted(self):
         """ Get deleted value.
 
           Notes:
-              Deleted is the time when the notification was deleted
+              Deleted marks if the entity has been deleted.
 
               
         """
@@ -131,11 +191,33 @@ class Notification(RESTObject):
         """ Set deleted value.
 
           Notes:
-              Deleted is the time when the notification was deleted
+              Deleted marks if the entity has been deleted.
 
               
         """
         self._deleted = value
+    
+    @property
+    def deletedAt(self):
+        """ Get deletedAt value.
+
+          Notes:
+              Deleted is the time when the notification was deleted
+
+              
+        """
+        return self._deletedat
+
+    @deletedAt.setter
+    def deletedAt(self, value):
+        """ Set deletedAt value.
+
+          Notes:
+              Deleted is the time when the notification was deleted
+
+              
+        """
+        self._deletedat = value
     
     @property
     def limit(self):
@@ -180,6 +262,28 @@ class Notification(RESTObject):
               
         """
         self._name = value
+    
+    @property
+    def namespace(self):
+        """ Get namespace value.
+
+          Notes:
+              Namespace tag attached to an entity
+
+              
+        """
+        return self._namespace
+
+    @namespace.setter
+    def namespace(self, value):
+        """ Set namespace value.
+
+          Notes:
+              Namespace tag attached to an entity
+
+              
+        """
+        self._namespace = value
     
     @property
     def new(self):
@@ -290,6 +394,94 @@ class Notification(RESTObject):
               
         """
         self._page = value
+    
+    @property
+    def parentID(self):
+        """ Get parentID value.
+
+          Notes:
+              ParentID is the ID of the parent, if any,
+
+              
+        """
+        return self._parentid
+
+    @parentID.setter
+    def parentID(self, value):
+        """ Set parentID value.
+
+          Notes:
+              ParentID is the ID of the parent, if any,
+
+              
+        """
+        self._parentid = value
+    
+    @property
+    def parentType(self):
+        """ Get parentType value.
+
+          Notes:
+              ParentType is the type of the parent, if any. It will be set to the parent's Identity.Name.
+
+              
+        """
+        return self._parenttype
+
+    @parentType.setter
+    def parentType(self, value):
+        """ Set parentType value.
+
+          Notes:
+              ParentType is the type of the parent, if any. It will be set to the parent's Identity.Name.
+
+              
+        """
+        self._parenttype = value
+    
+    @property
+    def status(self):
+        """ Get status value.
+
+          Notes:
+              Status of an entity
+
+              
+        """
+        return self._status
+
+    @status.setter
+    def status(self, value):
+        """ Set status value.
+
+          Notes:
+              Status of an entity
+
+              
+        """
+        self._status = value
+    
+    @property
+    def updatedAt(self):
+        """ Get updatedAt value.
+
+          Notes:
+              UpdatedAt is the time at which an entity was updated.
+
+              
+        """
+        return self._updatedat
+
+    @updatedAt.setter
+    def updatedAt(self, value):
+        """ Set updatedAt value.
+
+          Notes:
+              UpdatedAt is the time at which an entity was updated.
+
+              
+        """
+        self._updatedat = value
     
     def validate(self):
         """ Validate valides the current information stored into the structure.
