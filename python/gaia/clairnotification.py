4a4c2afda183757317695ad3e7c115a8ee8b51b7
Suresh Ramamurthy <suresh@aporeto.com>
2016-10-05 20:36:53 -0700

    Fixed: added base model to clairnotification

--- python/gaia/clairnotification.py
+++ python/gaia/clairnotification.py
@@ -29,7 +29,10 @@ class ClairNotification(RESTObject):
         # Read/Write Attributes
         
         self._id = None
+        self._annotation = None
+        self._associatedtags = None
         self._created = None
+        self._createdat = None
         self._deleted = None
         self._layerlimit = None
         self._layersintroducingnewvulnerability = None
@@ -44,9 +47,16 @@ class ClairNotification(RESTObject):
         self._oldvulnerabilitylink = None
         self._oldvulnerabilityname = None
         self._page = None
+        self._parentid = None
+        self._parenttype = None
+        self._status = None
+        self._updatedat = None
         
         self.expose_attribute(local_name="ID", remote_name="ID")
+        self.expose_attribute(local_name="annotation", remote_name="annotation")
+        self.expose_attribute(local_name="associatedTags", remote_name="associatedTags")
         self.expose_attribute(local_name="created", remote_name="created")
+        self.expose_attribute(local_name="createdAt", remote_name="createdAt")
         self.expose_attribute(local_name="deleted", remote_name="deleted")
         self.expose_attribute(local_name="layerlimit", remote_name="layerlimit")
         self.expose_attribute(local_name="layersIntroducingNewVulnerability", remote_name="layersIntroducingNewVulnerability")
@@ -61,6 +71,10 @@ class ClairNotification(RESTObject):
         self.expose_attribute(local_name="oldVulnerabilityLink", remote_name="oldVulnerabilityLink")
         self.expose_attribute(local_name="oldVulnerabilityName", remote_name="oldVulnerabilityName")
         self.expose_attribute(local_name="page", remote_name="page")
+        self.expose_attribute(local_name="parentID", remote_name="parentID")
+        self.expose_attribute(local_name="parentType", remote_name="parentType")
+        self.expose_attribute(local_name="status", remote_name="status")
+        self.expose_attribute(local_name="updatedAt", remote_name="updatedAt")
 
         self._compute_args(**kwargs)
 
@@ -110,6 +124,50 @@ class ClairNotification(RESTObject):
         self._id = value
     
     @property
+    def annotation(self):
+        """ Get annotation value.
+
+          Notes:
+              Annotation stores additional information about an entity
+
+              
+        """
+        return self._annotation
+
+    @annotation.setter
+    def annotation(self, value):
+        """ Set annotation value.
+
+          Notes:
+              Annotation stores additional information about an entity
+
+              
+        """
+        self._annotation = value
+    
+    @property
+    def associatedTags(self):
+        """ Get associatedTags value.
+
+          Notes:
+              AssociatedTags are the list of tags attached to an entity
+
+              
+        """
+        return self._associatedtags
+
+    @associatedTags.setter
+    def associatedTags(self, value):
+        """ Set associatedTags value.
+
+          Notes:
+              AssociatedTags are the list of tags attached to an entity
+
+              
+        """
+        self._associatedtags = value
+    
+    @property
     def created(self):
         """ Get created value.
 
@@ -130,6 +188,28 @@ class ClairNotification(RESTObject):
               
         """
         self._created = value
+    
+    @property
+    def createdAt(self):
+        """ Get createdAt value.
+
+          Notes:
+              CreatedAt is the time at which an entity was created
+
+              
+        """
+        return self._createdat
+
+    @createdAt.setter
+    def createdAt(self, value):
+        """ Set createdAt value.
+
+          Notes:
+              CreatedAt is the time at which an entity was created
+
+              
+        """
+        self._createdat = value
     
     @property
     def deleted(self):
@@ -438,6 +518,94 @@ class ClairNotification(RESTObject):
               
         """
         self._page = value
+    
+    @property
+    def parentID(self):
+        """ Get parentID value.
+
+          Notes:
+              ParentID is the ID of the parent, if any,
+
+              
+        """
+        return self._parentid
+
+    @parentID.setter
+    def parentID(self, value):
+        """ Set parentID value.
+
+          Notes:
+              ParentID is the ID of the parent, if any,
+
+              
+        """
+        self._parentid = value
+    
+    @property
+    def parentType(self):
+        """ Get parentType value.
+
+          Notes:
+              ParentType is the type of the parent, if any. It will be set to the parent's Identity.Name.
+
+              
+        """
+        return self._parenttype
+
+    @parentType.setter
+    def parentType(self, value):
+        """ Set parentType value.
+
+          Notes:
+              ParentType is the type of the parent, if any. It will be set to the parent's Identity.Name.
+
+              
+        """
+        self._parenttype = value
+    
+    @property
+    def status(self):
+        """ Get status value.
+
+          Notes:
+              Status of an entity
+
+              
+        """
+        return self._status
+
+    @status.setter
+    def status(self, value):
+        """ Set status value.
+
+          Notes:
+              Status of an entity
+
+              
+        """
+        self._status = value
+    
+    @property
+    def updatedAt(self):
+        """ Get updatedAt value.
+
+          Notes:
+              UpdatedAt is the time at which an entity was updated.
+
+              
+        """
+        return self._updatedat
+
+    @updatedAt.setter
+    def updatedAt(self, value):
+        """ Set updatedAt value.
+
+          Notes:
+              UpdatedAt is the time at which an entity was updated.
+
+              
+        """
+        self._updatedat = value
     
     def validate(self):
         """ Validate valides the current information stored into the structure.
