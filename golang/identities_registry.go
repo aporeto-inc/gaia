a620103d7defdd4abf597c3058c2cd6187642481
Dimitri Stiliadis <dstiliadis@gmail.com>
2017-01-09 12:05:24 -0800

    Codegen

--- golang/identities_registry.go
+++ golang/identities_registry.go
@@ -41,7 +41,6 @@ func init() {
 	elemental.RegisterIdentity(UserIdentity)
 }
 
-// IdentifiableForIdentity returns a new instance of the Identifiable for the given identity name.
 func IdentifiableForIdentity(identity string) elemental.Identifiable {
 
 	switch identity {
@@ -118,4 +117,6 @@ func IdentifiableForIdentity(identity string) elemental.Identifiable {
 	default:
 		return nil
 	}
+
+	return nil
 }
