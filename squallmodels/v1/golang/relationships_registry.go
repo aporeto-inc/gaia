2fbbdfc345de3f9bea2572c03f64e95e0649a9c0
Dimitri Stiliadis <dstiliadis@gmail.com>
2018-01-19 11:47:51 -0800

    Fix create

--- squallmodels/v1/golang/relationships_registry.go
+++ squallmodels/v1/golang/relationships_registry.go
@@ -528,24 +528,6 @@ func init() {
 		AllowsCreate: map[string]bool{
 			"root": true,
 		},
-		AllowsUpdate: map[string]bool{
-			"root": true,
-		},
-		AllowsPatch: map[string]bool{
-			"root": true,
-		},
-		AllowsDelete: map[string]bool{
-			"root": true,
-		},
-		AllowsRetrieve: map[string]bool{
-			"root": true,
-		},
-		AllowsRetrieveMany: map[string]bool{
-			"root": true,
-		},
-		AllowsInfo: map[string]bool{
-			"root": true,
-		},
 	}
 
 	relationshipsRegistry[SuggestedPolicyIdentity] = &elemental.Relationship{
