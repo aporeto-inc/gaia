81b0876a66ae54682568dfde29f4d7662d8d066e
Satyam Sinha <satyam@aporeto.com>
2018-02-23 13:37:01 -0800

    New: invoice record

--- v1/golang/types/invoicerecord.go
+++ v1/golang/types/invoicerecord.go
@@ -1,12 +1,12 @@
 package types
 
-// InvoiceRecordDetail is a service associated with an enforcer profile for a host.
-type InvoiceRecordDetail struct {
+// InvoiceRecordDetails is a service associated with an enforcer profile for a host.
+type InvoiceRecordDetails struct {
 	TimeStamp string `json:"timestamp" bson:"timestamp" mapstructure:"timestamp,omitempty"`
 }
 
 // InvoiceRecordDetailsList is a list of ProcessingUnitServices
-type InvoiceRecordDetailsList []*InvoiceRecordDetail
+type InvoiceRecordDetailsList []*InvoiceRecordDetails
 
 // Validate will validate the types. TODO: Do we need a stub if there is nothing to validate ?
 func (i InvoiceRecordDetailsList) Validate() error {
