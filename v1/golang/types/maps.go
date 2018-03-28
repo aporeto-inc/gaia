21a2cdba6a6d448d3e96c6bd1f5db3baa2f89373
Christophe Serafin <christophe.serafin@gmail.com>
2018-03-28 12:51:50 -0700

    Optimized version of the array using an init function

--- v1/golang/types/maps.go
+++ v1/golang/types/maps.go
@@ -101,151 +101,157 @@ func NewGraphGroup() *GraphGroup {
 
 // l4Protocols is te list of all IANA protocols
 // (Ref https://en.wikipedia.org/wiki/List_of_IP_protocol_numbers)
-var l4Protocols = map[int]string{
-	0:   "HOPOPT",
-	1:   "ICMP",
-	2:   "IGMP",
-	3:   "GGP",
-	4:   "IP-in-IP",
-	5:   "ST",
-	6:   "TCP",
-	7:   "CBT",
-	8:   "EGP",
-	9:   "IGP",
-	10:  "BBN-RCC-MON",
-	11:  "NVP-II",
-	12:  "PUP",
-	13:  "ARGUS",
-	14:  "EMCON",
-	15:  "XNET",
-	16:  "CHAOS",
-	17:  "UDP",
-	18:  "MUX",
-	19:  "DCN-MEAS",
-	20:  "HMP",
-	21:  "PRM",
-	22:  "XNS-IDP",
-	23:  "TRUNK-1",
-	24:  "TRUNK-2",
-	25:  "LEAF-1",
-	26:  "LEAF-2",
-	27:  "RDP",
-	28:  "IRTP",
-	29:  "ISO-TP4",
-	30:  "NETBLT",
-	31:  "MFE-NSP",
-	32:  "MERIT-INP",
-	33:  "DCCP",
-	34:  "3PC",
-	35:  "IDPR",
-	36:  "XTP",
-	37:  "DDP",
-	38:  "IDPR-CMTP",
-	39:  "TP++",
-	40:  "IL",
-	41:  "IPv6",
-	42:  "SDRP",
-	43:  "IPv6-Route",
-	44:  "IPv6-Frag",
-	45:  "IDRP",
-	46:  "RSVP",
-	47:  "GREs",
-	48:  "DSR",
-	49:  "BNA",
-	50:  "ESP",
-	51:  "AH",
-	52:  "I-NLSP",
-	53:  "SWIPE",
-	54:  "NARP",
-	55:  "MOBILE",
-	56:  "TLSP",
-	57:  "SKIP",
-	58:  "IPv6-ICMP",
-	59:  "IPv6-NoNxt",
-	60:  "IPv6-Opts",
-	62:  "CFTP",
-	64:  "SAT-EXPAK",
-	65:  "KRYPTOLAN",
-	66:  "RVD",
-	67:  "IPPC",
-	69:  "SAT-MON",
-	70:  "VISA",
-	71:  "IPCU",
-	72:  "CPNX",
-	73:  "CPHB",
-	74:  "WSN",
-	75:  "PVP",
-	76:  "BR-SAT-MON",
-	77:  "SUN-ND",
-	78:  "WB-MON",
-	79:  "WB-EXPAK",
-	80:  "ISO-IP",
-	81:  "VMTP",
-	82:  "SECURE-VMTP",
-	83:  "VINES",
-	84:  "TTP",
-	85:  "NSFNET-IGP",
-	86:  "DGP",
-	87:  "TCF",
-	88:  "EIGRP",
-	89:  "OSPF",
-	90:  "Sprite-RPC",
-	91:  "LARP",
-	92:  "MTP",
-	93:  "AX.25",
-	94:  "OS",
-	95:  "MICP",
-	96:  "SCC-SP",
-	97:  "ETHERIP",
-	98:  "ENCAP",
-	100: "GMTP",
-	101: "IFMP",
-	102: "PNNI",
-	103: "PIM",
-	104: "ARIS",
-	105: "SCPS",
-	106: "QNX",
-	107: "A/N",
-	108: "IPComp",
-	109: "SNP",
-	110: "Compaq-Peer",
-	111: "IPX-in-IP",
-	112: "VRRP",
-	113: "PGM",
-	115: "L2TP",
-	116: "DDX",
-	117: "IATP",
-	118: "STP",
-	119: "SRP",
-	120: "UTI",
-	121: "SMP",
-	122: "SM",
-	123: "PTP",
-	124: "IS-IS over IPv4",
-	125: "FIRE",
-	126: "CRTP",
-	127: "CRUDP",
-	128: "SSCOPMCE",
-	129: "IPLT",
-	130: "SPS",
-	131: "PIPE",
-	132: "SCTP",
-	133: "FC",
-	134: "RSVP-E2E-IGNORE",
-	135: "Mobility Header",
-	136: "UDPLite",
-	137: "MPLS-in-IP",
-	138: "manet",
-	139: "HIP",
-	140: "Shim6",
-	141: "WESP",
-	142: "ROHC",
+var l4Protocols = make([]string, 150)
+
+func init() {
+	l4Protocols[0] = "HOPOPT"
+	l4Protocols[1] = "ICMP"
+	l4Protocols[2] = "IGMP"
+	l4Protocols[3] = "GGP"
+	l4Protocols[4] = "IP-in-IP"
+	l4Protocols[5] = "ST"
+	l4Protocols[6] = "TCP"
+	l4Protocols[7] = "CBT"
+	l4Protocols[8] = "EGP"
+	l4Protocols[9] = "IGP"
+	l4Protocols[10] = "BBN-RCC-MON"
+	l4Protocols[11] = "NVP-II"
+	l4Protocols[12] = "PUP"
+	l4Protocols[13] = "ARGUS"
+	l4Protocols[14] = "EMCON"
+	l4Protocols[15] = "XNET"
+	l4Protocols[16] = "CHAOS"
+	l4Protocols[17] = "UDP"
+	l4Protocols[18] = "MUX"
+	l4Protocols[19] = "DCN-MEAS"
+	l4Protocols[20] = "HMP"
+	l4Protocols[21] = "PRM"
+	l4Protocols[22] = "XNS-IDP"
+	l4Protocols[23] = "TRUNK-1"
+	l4Protocols[24] = "TRUNK-2"
+	l4Protocols[25] = "LEAF-1"
+	l4Protocols[26] = "LEAF-2"
+	l4Protocols[27] = "RDP"
+	l4Protocols[28] = "IRTP"
+	l4Protocols[29] = "ISO-TP4"
+	l4Protocols[30] = "NETBLT"
+	l4Protocols[31] = "MFE-NSP"
+	l4Protocols[32] = "MERIT-INP"
+	l4Protocols[33] = "DCCP"
+	l4Protocols[34] = "3PC"
+	l4Protocols[35] = "IDPR"
+	l4Protocols[36] = "XTP"
+	l4Protocols[37] = "DDP"
+	l4Protocols[38] = "IDPR-CMTP"
+	l4Protocols[39] = "TP++"
+	l4Protocols[40] = "IL"
+	l4Protocols[41] = "IPv6"
+	l4Protocols[42] = "SDRP"
+	l4Protocols[43] = "IPv6-Route"
+	l4Protocols[44] = "IPv6-Frag"
+	l4Protocols[45] = "IDRP"
+	l4Protocols[46] = "RSVP"
+	l4Protocols[47] = "GREs"
+	l4Protocols[48] = "DSR"
+	l4Protocols[49] = "BNA"
+	l4Protocols[50] = "ESP"
+	l4Protocols[51] = "AH"
+	l4Protocols[52] = "I-NLSP"
+	l4Protocols[53] = "SWIPE"
+	l4Protocols[54] = "NARP"
+	l4Protocols[55] = "MOBILE"
+	l4Protocols[56] = "TLSP"
+	l4Protocols[57] = "SKIP"
+	l4Protocols[58] = "IPv6-ICMP"
+	l4Protocols[59] = "IPv6-NoNxt"
+	l4Protocols[60] = "IPv6-Opts"
+	l4Protocols[62] = "CFTP"
+	l4Protocols[64] = "SAT-EXPAK"
+	l4Protocols[65] = "KRYPTOLAN"
+	l4Protocols[66] = "RVD"
+	l4Protocols[67] = "IPPC"
+	l4Protocols[69] = "SAT-MON"
+	l4Protocols[70] = "VISA"
+	l4Protocols[71] = "IPCU"
+	l4Protocols[72] = "CPNX"
+	l4Protocols[73] = "CPHB"
+	l4Protocols[74] = "WSN"
+	l4Protocols[75] = "PVP"
+	l4Protocols[76] = "BR-SAT-MON"
+	l4Protocols[77] = "SUN-ND"
+	l4Protocols[78] = "WB-MON"
+	l4Protocols[79] = "WB-EXPAK"
+	l4Protocols[80] = "ISO-IP"
+	l4Protocols[81] = "VMTP"
+	l4Protocols[82] = "SECURE-VMTP"
+	l4Protocols[83] = "VINES"
+	l4Protocols[84] = "TTP"
+	l4Protocols[85] = "NSFNET-IGP"
+	l4Protocols[86] = "DGP"
+	l4Protocols[87] = "TCF"
+	l4Protocols[88] = "EIGRP"
+	l4Protocols[89] = "OSPF"
+	l4Protocols[90] = "Sprite-RPC"
+	l4Protocols[91] = "LARP"
+	l4Protocols[92] = "MTP"
+	l4Protocols[93] = "AX.25"
+	l4Protocols[94] = "OS"
+	l4Protocols[95] = "MICP"
+	l4Protocols[96] = "SCC-SP"
+	l4Protocols[97] = "ETHERIP"
+	l4Protocols[98] = "ENCAP"
+	l4Protocols[100] = "GMTP"
+	l4Protocols[101] = "IFMP"
+	l4Protocols[102] = "PNNI"
+	l4Protocols[103] = "PIM"
+	l4Protocols[104] = "ARIS"
+	l4Protocols[105] = "SCPS"
+	l4Protocols[106] = "QNX"
+	l4Protocols[107] = "A/N"
+	l4Protocols[108] = "IPComp"
+	l4Protocols[109] = "SNP"
+	l4Protocols[110] = "Compaq-Peer"
+	l4Protocols[111] = "IPX-in-IP"
+	l4Protocols[112] = "VRRP"
+	l4Protocols[113] = "PGM"
+	l4Protocols[115] = "L2TP"
+	l4Protocols[116] = "DDX"
+	l4Protocols[117] = "IATP"
+	l4Protocols[118] = "STP"
+	l4Protocols[119] = "SRP"
+	l4Protocols[120] = "UTI"
+	l4Protocols[121] = "SMP"
+	l4Protocols[122] = "SM"
+	l4Protocols[123] = "PTP"
+	l4Protocols[124] = "IS-IS over IPv4"
+	l4Protocols[125] = "FIRE"
+	l4Protocols[126] = "CRTP"
+	l4Protocols[127] = "CRUDP"
+	l4Protocols[128] = "SSCOPMCE"
+	l4Protocols[129] = "IPLT"
+	l4Protocols[130] = "SPS"
+	l4Protocols[131] = "PIPE"
+	l4Protocols[132] = "SCTP"
+	l4Protocols[133] = "FC"
+	l4Protocols[134] = "RSVP-E2E-IGNORE"
+	l4Protocols[135] = "Mobility Header"
+	l4Protocols[136] = "UDPLite"
+	l4Protocols[137] = "MPLS-in-IP"
+	l4Protocols[138] = "manet"
+	l4Protocols[139] = "HIP"
+	l4Protocols[140] = "Shim6"
+	l4Protocols[141] = "WESP"
+	l4Protocols[142] = "ROHC"
 }
 
 // ProtocolName returns the IANA for the protocol
 func ProtocolName(n int) string {
 
-	if protocol, ok := l4Protocols[n]; ok {
+	if n < 0 || n > len(l4Protocols) {
+		return "N/A"
+	}
+
+	if protocol := l4Protocols[n]; protocol != "" {
 		return protocol
 	}
 
