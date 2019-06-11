2f7cc3dbf95c0b2ee2449c9c6343b936d24e42f1
eric <eric@aporeto.com>
2019-06-10 18:02:12 -0700

    Update: Correct RelationApplyObjectOnly string to be uniform with others

--- constants/constants.go
+++ constants/constants.go
@@ -207,8 +207,8 @@ const (
 	// RelationApplySubject is the system create relation=apply-subject
 	RelationApplySubjectOnly = "relation=apply-subject"
 
-	// RelationApplyObject is the system create relation-apply-object
-	RelationApplyObjectOnly = "relation-apply=object"
+	// RelationApplyObject is the system create relation=apply-object
+	RelationApplyObjectOnly = "relation=apply-object"
 
 	// ActionAllowTagString is the system created action=allow tag.
 	ActionAllowTagString = "action=allow"
