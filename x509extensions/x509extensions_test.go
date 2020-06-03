bd89f91d1a7875f88ba9bde91aaf71cbd9d831a6
Satyam Sinha <sasinha@paloaltonetworks.com>
2020-06-03 00:20:46 -0700

    Added unit tests for clone and consolidated from others

--- x509extensions/x509extensions_test.go
+++ x509extensions/x509extensions_test.go
@@ -6,29 +6,50 @@ import (
 	"testing"
 )
 
-func TestIdentityTags(t *testing.T) {
+func Test_clone(t *testing.T) {
+	type args struct {
+		in asn1.ObjectIdentifier
+	}
 	tests := []struct {
-		name    string
-		clobber bool
-		want    asn1.ObjectIdentifier
+		name string
+		args args
+		want asn1.ObjectIdentifier
 	}{
 		{
-			name:    "sanity",
-			clobber: false,
-			want:    asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 50798, 1, 1},
-		},
-		{
-			name:    "check constant",
-			clobber: true,
-			want:    asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 50798, 1, 1},
+			name: "sanity",
+			args: args{
+				in: asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 50798, 1, 1},
+			},
+			want: asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 50798, 1, 1},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if tt.clobber {
-				c := IdentityTags()
-				c[0] = 0
+			got := clone(tt.args.in)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("clone() = %v, want %v", got, tt.want)
 			}
+			// Modify got and make sure input was not clobbered.
+			got[0] = 0
+			if !reflect.DeepEqual(tt.args.in, tt.want) {
+				t.Errorf("clone() clobber detected = %v, want %v", tt.args.in, tt.want)
+			}
+		})
+	}
+}
+
+func TestIdentityTags(t *testing.T) {
+	tests := []struct {
+		name string
+		want asn1.ObjectIdentifier
+	}{
+		{
+			name: "sanity",
+			want: asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 50798, 1, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 			if got := IdentityTags(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("IdentityTags() = %v, want %v", got, tt.want)
 			}
@@ -38,26 +59,15 @@ func TestIdentityTags(t *testing.T) {
 
 func TestController(t *testing.T) {
 	tests := []struct {
-		name    string
-		clobber bool
-		want    asn1.ObjectIdentifier
+		name string
+		want asn1.ObjectIdentifier
 	}{
 		{
-			name:    "sanity",
-			clobber: false,
-			want:    asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 50798, 1, 2},
-		},
-		{
-			name:    "check constant",
-			clobber: true,
-			want:    asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 50798, 1, 2},
+			name: "sanity",
+			want: asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 50798, 1, 2},
 		},
 	}
 	for _, tt := range tests {
-		if tt.clobber {
-			c := Controller()
-			c[0] = 0
-		}
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Controller(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Controller() = %v, want %v", got, tt.want)
